35e22c06d6da33f4357746e9723870d53babf343
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2025-01-23 16:46:44 +0000

    Fix #4

--- soc_guess_who/src/assets/components/Hero/Hero.jsx
+++ soc_guess_who/src/assets/components/Hero/Hero.jsx
@@ -2,10 +2,12 @@ import DataInput from "../DataInput/DataInput";
 import SubmitButton from "../SubmitButton/SubmitButton";
 import CardDisplay from "../CardDisplay/CardDisplay";
 
+
 export default function Hero() {
   return (
     <div>
       <div className="createDataSection">
+        <h1>Hero Section</h1>
         <DataInput />
         <SubmitButton />
       </div>
