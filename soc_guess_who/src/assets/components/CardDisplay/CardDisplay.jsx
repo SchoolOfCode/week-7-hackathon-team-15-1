e7a7054216a3c2a1c06cebde41c817cd44b8431a
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2025-01-23 18:41:20 +0000

    Fix #5

--- soc_guess_who/src/assets/components/CardDisplay/CardDisplay.jsx
+++ soc_guess_who/src/assets/components/CardDisplay/CardDisplay.jsx
@@ -1,11 +1,28 @@
 import FlashCard from "../FlashCard/FlashCard";
 
+import { useState } from "react";
+
+
+
+
+
 const profile = { name: "Hope", fact: "Has a dog named porridge" };
 
 export default function CardDisplay() {
+
+ const [isFront, setIsFront] = useState(true);
+
+
+  function handleClick() {
+    setIsFront(!isFront);
+  }
+
+
   return (
     <div>
-      <FlashCard name={profile.name} fact={profile.fact} />
+      <FlashCard onFlashCardClick={handleClick} name={profile.name} fact={profile.fact} panel={isFront ? "Front" : "Back" } />
     </div>
   );
 }
+
+
--- soc_guess_who/src/assets/components/FlashCard/FlashCard.jsx
+++ soc_guess_who/src/assets/components/FlashCard/FlashCard.jsx
@@ -1,9 +1,25 @@
-export default function FlashCard({ name, fact }) {
-  return (
-    <div>
+import styles from "./FlashCard.module.css";
+
+
+
+export default function FlashCard({ name, fact, panel, onFlashCardClick}) {
+  if (panel == "Front"){
+     return (
+    <div className={styles.FlashCard} onClick={onFlashCardClick}>
       <h1>🔍</h1>
       <h3>{fact}</h3>
     </div>
   );
+  }
+  else {
+    return (
+    <div onClick={onFlashCardClick}>
+      <h1>👍</h1>
+      <h3>{name}</h3>
+    </div>
+    );
+  }
+
+ 
 }
-//I used to be a music journalist
+
